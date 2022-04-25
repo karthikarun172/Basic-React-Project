f839519c158d86963d1f34ca6853e374f70ab5ae
arun <karthikarun172@gmail.com>
2022-04-25 15:14:57 +0530

    display data

--- src/App.js
+++ src/App.js
@@ -21,48 +21,13 @@ function App() {
   ]
 
   const [Data, setData] = useState(Swiggy)
-  const [AllData, setAllData] = useState(Swiggy)
 
-  const handleFilter = (filterValue) => {
-    const value = [...Swiggy];
-    if (filterValue === "all") {
-      setData(AllData)
-      return
-    }
-    const filtering = value.filter(f => f.cuisine === filterValue)
-    setData(filtering)
 
-  }
-
-  const handleSort = (distance) => {
-    const distanceValue = [...Swiggy]
-    if (distance === "near") {
-      const distanceNear = distanceValue.sort((a, b) => a.distance - a.distance)
-      setData(distanceNear)
-      return
-    }
-    const distanceFar = distanceValue.sort((a, b) => b.distance - a.distance)
-    setData(distanceFar)
-
-  }
 
   return (
     <div className='mainContainer' >
       <h1>Hotels near you</h1>
-      <p>filters</p>
-      <ul className='filterAndSortContainer' >
-        <li onClick={() => handleFilter("all")} >all</li>
-        <li onClick={() => handleFilter("Indian")} >Indian</li>
-        <li onClick={() => handleFilter("Mexican")} >Mexican</li>
-        <li onClick={() => handleFilter("American")} > American</li>
-      </ul>
-
-      <p>sort</p>
-      <ul className='filterAndSortContainer' >
-        <li onClick={() => handleSort("near")} >near to far</li>
-        <li onClick={() => handleSort("far")} >far to near</li>
-      </ul>
-      <div style={{ display: "flex", justifyContent: "space-between", flexWrap: "wrap", width: "100%" }}>
+      <div className='CaroselContainer'>
         {Data.map(d => (
           <CaroselCards name={d.hotel_Name} cusinie={d.cuisine} rating={d.rating} />
         ))}
